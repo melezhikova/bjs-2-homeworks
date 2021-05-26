b6b127d409ef0daa71a89fb53bf3f66172e9f9d8
Liudmila Melezhikova <melezhikova@yandex.ru>
2021-05-26 19:08:01 +0500

    first commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,26 +1,47 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  avg = (sum / arr.length).toFixed(2);
 
   return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
-
-  // Ваш кода
-  // for ...
   
+  let max = func(arrOfArr[0]);
+  console.log(max);
+  
+  for (i = 0; i < arrOfArr.length; i++) {
+    sum = func(arrOfArr[i]);       
+    if (sum > max) {
+    max = sum;
+    }
+  }
+
   return max
 }
