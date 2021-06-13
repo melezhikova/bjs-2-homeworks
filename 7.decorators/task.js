57b30ace086874aac940688ae6d291d018ad76dc
Liudmila Melezhikova <melezhikova@yandex.ru>
2021-06-13 18:15:10 +0500

    first commit variables

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,5 +1,19 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+    const hash = args.join(",");
+    if (hash in cache) {
+      console.log("Из кэша: " + cache(hash));
+    } else {
+      let result = func(...args);
+      if (cache.keys(length) === 5) {
+
+      } cache[hash] = result;
+      console.log("Вычисляем: " + result);
+    };
+  }
+
+  return wrapper;
 }
 
 function debounceDecoratorNew(func) {
