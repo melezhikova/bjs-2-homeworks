a9847a05e0d034b0778d859cadc8d4ba6da8d287
Liudmila Melezhikova <melezhikova@yandex.ru>
2021-06-13 22:45:24 +0500

    second commit changes

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,26 +1,62 @@
 function cachingDecoratorNew(func) {
   let cache = [];
+
   function wrapper(...args) {
     const hash = args.join(",");
-    if (hash in cache) {
-      console.log("Из кэша: " + cache(hash));
+    let idx = cache.findIndex((item) => item.hash === hash);
+    if (idx !== -1) {
+      console.log("Из кэша: " + cache[idx].value);
+      return "Из кэша: " + cache[idx].value;
     } else {
       let result = func(...args);
-      if (cache.keys(length) === 5) {
-
-      } cache[hash] = result;
+      cache.push({hash: hash, value: result});
+      if (cache.length > 5) {
+        cache.shift();
+      } 
       console.log("Вычисляем: " + result);
-    };
+      return "Вычисляем: " + result;
+    }
   }
-
+  
   return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  func();
+  let isTrottled = true, timeout;
+  
+  return function() {
+    if (!isTrottled) {
+      return;
+    }
+    clearTimeout(timeout);
+    isTrottled = false;
+    timeout = setTimeout(() => {
+      func();
+     }, ms);
+
+   };
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  func();
+  let isTrottled = true, timeout;
+    
+  function wrapper() {
+    wrapper.history.push("функция вызвана");
+    let count = wrapper.history.length;
+    if (!isTrottled) {
+      return;
+    }
+    clearTimeout(timeout);
+    isTrottled = false;
+    timeout = setTimeout(() => {
+      func();
+     }, ms);
+     console.log(count);
+   };
+
+   wrapper.history = [];
+   return wrapper;
 }
